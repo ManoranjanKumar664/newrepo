a5733b6404d12abb132fda66be432a46fb1730d3
Manoranjan Kumar <112553053+ManoranjanKumar664@users.noreply.github.com>
2022-12-11 14:33:41 +0530

    Update one.java

--- one.java
+++ one.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+import java.lang.Scanner;
 
+class sum
+{
+  int a, b, c;
+  void input()
+  {
+    System.out.println("enter any two number");
+    Scanner mano=new Scanner();
+    a=mano.nextInt();
+    b=mano.nextInt();
+  }
+  
+  void output()
+  {
+    c=a+b;
+    System.out.println("sum="+c);
+  }
+}
+
+class one
+{
+  public static void main(String[] args)
+  {
+    Scanner ob=new Scanner();
+    ob.input();
+    ob.output();
+  }
+}
