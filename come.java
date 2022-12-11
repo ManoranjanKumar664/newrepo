cc86d310ee6efa52ccc6c0713a88bcef055354ff
Manoranjan Kumar <112553053+ManoranjanKumar664@users.noreply.github.com>
2022-12-11 15:42:34 +0530

    Add files via upload
    some files added

---
+++ come.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.lang.System;
+
+class name
+{
+  int a, b, c;
+ void input(int x, int y)
+{ 
+a=x;
+b=y;
+}
+
+void output()
+{
+System.out.println(a+"  "+b);
+}
+
+}
+
+
+class come
+{
+ public static void main(String[] args)
+{
+ Scanner doitnow=new Scanner(100, 200);
+doitnow.output();
+}
+}
